82d98d27d117c380419ca6de427c72acce80a41d
Ardian Hadiyanto <ardianhadiyanto@gmail.com>
2020-05-16 16:24:46 -0500

    Implemented a simple error handling on PlantsController

--- src/main/kotlin/io/hadiyanto/planlo/controllers/PlantsController.kt
+++ src/main/kotlin/io/hadiyanto/planlo/controllers/PlantsController.kt
@@ -1,10 +1,14 @@
 package io.hadiyanto.planlo.controllers
 
 import io.hadiyanto.planlo.entities.Geolocation
+import io.hadiyanto.planlo.entities.Plant
 import io.hadiyanto.planlo.services.PlantsService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/plants")
@@ -12,5 +16,11 @@ class PlantsController(
   private val plantsService: PlantsService
 ) {
   @GetMapping
-  fun getPlantsFor(geolocation: Geolocation) = plantsService.getPlantsFor(geolocation)
+  fun getPlantsFor(geolocation: Geolocation): ResponseEntity<List<Plant>> {
+    return try {
+      ResponseEntity(plantsService.getPlantsFor(geolocation), HttpStatus.OK)
+    } catch (e: RuntimeException) {
+      throw ResponseStatusException(HttpStatus.NOT_FOUND, "Foo Not Found", e)
+    }
+  }
 }
